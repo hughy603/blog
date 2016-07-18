c021e7c9cb37851adb85b64daedaafa62423fd19
Adam Rice <rice.adam@gmail.com>
2016-07-18 01:40:48 -0400

    Added aws setup guide

---
+++ AWS Setup.md
@@ -0,0 +1,4 @@
+# Deploy Application To AWS
+
+## Set Environment Variables through consol
+http://stackoverflow.com/questions/11211007/how-do-you-pass-custom-environment-variable-on-amazon-elastic-beanstalk-aws-ebs
--- blog/config/settings/production.py
+++ blog/config/settings/production.py
@@ -166,7 +166,7 @@ CACHES = {
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             'IGNORE_EXCEPTIONS': True,  # mimics memcache behavior.
-                                        # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
+            # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
         }
     }
 }
